aecf0949042cf71865396e9a5fc3803c651371bb
Thien Tran <gau.nernst@yahoo.com.sg>
2022-01-23 22:06:36 +0800

    fix numpy centernet decode

--- kv260_centernet/decode.py
+++ kv260_centernet/decode.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.ndimage.filters import maximum_filter
 
 
-def maxpool(x, kernel=(3,3), fill=0.):
+def maxpool(x: np.ndarray, kernel=3, fill=0.):
     return maximum_filter(x, size=kernel, mode='constant', cval=fill)
 
 def decode(heatmap: np.ndarray, box_offsets: np.ndarray, num_detections=100):
@@ -13,27 +13,24 @@ def decode(heatmap: np.ndarray, box_offsets: np.ndarray, num_detections=100):
     heatmap = heatmap * mask
 
     # get the best labels at each heatmap location
-    heatmap = heatmap.reshape(-1, n_classes)                        # flatten (H, W, C) -> (HW, C)
-    labels = np.argmax(heatmap, axis=-1)                            # (HW,)
-    heatmap = np.take_along_axis(heatmap, labels[:,None], axis=-1)  # (HW,)
+    heatmap = heatmap.reshape(-1, n_classes)    # flatten (H, W, C) -> (HW, C)
+    labels = np.argmax(heatmap, axis=-1)        # (HW,)
+    heatmap = np.max(heatmap, axis=-1)          # (HW,)
 
     # get topk. can also use heap here to have O(n) topk
-    indices = np.argpartition(heatmap, num_detections)
+    indices = np.argsort(-heatmap)[:num_detections]
     scores = heatmap[indices]
     labels = labels[indices]
 
     # decode boxes
     cx = indices % img_w + 0.5
     cy = indices // img_w + 0.5
-    box_offsets *= 16
+    box_offsets = box_offsets * 16
     box_offsets = box_offsets.reshape(-1, 4)
 
-    x1 = cx - box_offsets[indices,0]
-    y1 = cy - box_offsets[indices,1]
-    x2 = cx + box_offsets[indices,2]
-    y2 = cy + box_offsets[indices,3]
-
-    boxes = np.stack((x1, y1, x2, y2), axis=-1)
+    boxes = np.stack((cx,cy,cx,cy), axis=-1)
+    boxes[...,:2] -= box_offsets[indices,:2]
+    boxes[...,2:] += box_offsets[indices,2:]
     boxes *= 4
     
     return boxes, scores, labels
