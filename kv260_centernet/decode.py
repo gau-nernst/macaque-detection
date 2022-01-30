e2e4fcbba2745b2bc6df2626f396e4f2ff0d4494
Thien Tran <gau.nernst@yahoo.com.sg>
2022-01-30 19:38:30 +0800

    add nms and improve maxpool

--- kv260_centernet/decode.py
+++ kv260_centernet/decode.py
@@ -1,7 +1,8 @@
 import numpy as np
 # from scipy.ndimage.filters import maximum_filter
+# from numba import jit
 
-
+# @jit(nopython=True, parallel=True)
 def maxpool(a: np.ndarray, kernel=3):
     # return maximum_filter(a, size=kernel, mode='constant', cval=0)
     h, w, c = a.shape
@@ -9,11 +10,9 @@ def maxpool(a: np.ndarray, kernel=3):
     a = a.copy()
     padded_a = np.zeros((h+pad*2, w+pad*2, c), dtype=a.dtype)
     padded_a[pad:-pad,pad:-pad,:] = a
-    for i in range(c):
-        for i_x in range(pad, w+pad):
-            for i_y in range(pad, h+pad):
-                a[i_y-pad,i_x-pad,i] = np.max(padded_a[i_y-pad:i_y+pad+1,i_x-pad:i_x+pad+1,i])
-
+    for i_y in range(h):
+        for i_x in range(w):
+            np.max(padded_a[i_y:i_y+kernel,i_x:i_x+kernel], axis=(0,1), out=a[i_y,i_x])
     return a
 
 def decode(heatmap: np.ndarray, box_offsets: np.ndarray, num_detections=100):
@@ -45,3 +44,31 @@ def decode(heatmap: np.ndarray, box_offsets: np.ndarray, num_detections=100):
     boxes *= 4
     
     return boxes, scores, labels
+
+# @jit(nopython=True, parallel=True)
+def nms(boxes: np.ndarray, scores: np.ndarray, threshold: float):
+    indices = np.argsort(-scores)
+    areas = (boxes[...,2] - boxes[...,0]) * (boxes[...,3] - boxes[...,1])
+    
+    num_boxes = boxes.shape[0]
+    keep = np.zeros(num_boxes, dtype=np.uint8)
+    remove = np.zeros(num_boxes, dtype=np.uint8)
+    for i, idx in enumerate(indices):
+        if remove[idx]:
+            continue
+        keep[idx] = 1
+        
+        for idx2 in indices[i+1:]:
+            if remove[idx2]:
+                continue
+            ix1 = max(boxes[idx,0], boxes[idx2,0])
+            iy1 = max(boxes[idx,1], boxes[idx2,1])
+            ix2 = min(boxes[idx,2], boxes[idx2,2])
+            iy2 = min(boxes[idx,3], boxes[idx2,3])
+
+            inter = max(0, ix2-ix1) * max(0, iy2-iy1)
+            iou = inter / (areas[idx] + areas[idx2] - inter)
+            if iou > threshold:
+                remove[idx2] = 1
+
+    return np.where(keep)[0]
